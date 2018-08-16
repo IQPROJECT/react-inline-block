191fec3595a28b2781ef8f8da3b00b1b429f45b1
erisnuts <eris909@mail.ru>
2018-08-16 16:26:02 +0500

    [add] react-create-class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 var React = require('react');
 var assign = require('object-assign');
+var createReactClass = require('create-react-class');
 
 var inlineBlockStyle = {
   display: 'inline-block'
 }
 
-module.exports = React.createClass({
+module.exports = createReactClass({
 
   displayName: 'ReactInlineBlock',
 
-  render(){
-    const style = assign({}, this.props.style, inlineBlockStyle)
-    const props = assign({}, this.props, { style: style })
+  render() {
+    const style = assign({}, this.props.style, inlineBlockStyle);
+    const props = assign({}, this.props, { style: style });
 
-    return <div {...props}/>
+    return (<div {...props} />)
   }
-})
+});
